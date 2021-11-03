9f2df90b00baa8875c0ef725fc7f4cd58b486dd9
Marcelo Matos dos Santos <marcelo.santos@sanesul.ms.gov.br>
2021-11-03 17:25:17 -0400

    Novas Implementações Projeto

---
+++ Bernhoeft/ApiBernhoeft/Repository/PessoaRepository.cs
@@ -0,0 +1,19 @@
+﻿using ativ.Domain.Entities;
+using Ativ.Infra.Data.Repository;
+using Ativ.Infra.Data.Transactions;
+
+namespace ApiBernhoeft.Repository
+{
+    public interface IPessoaRepository : IRepository<Pessoa>
+    {
+    }
+    public class PessoaRepository : Repository<Pessoa>, IPessoaRepository
+    {
+        private IUnitOfWork _unitOfWork;
+
+        public PessoaRepository(IUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+    }
+}
--- Bernhoeft/CloudHttpFunction/Function.cs
+++ Bernhoeft/CloudHttpFunction/Function.cs
@@ -1,5 +1,8 @@
+using ApiBernhoeft.Repository;
 using ativ.Domain.Entities;
+using Ativ.Infra.Data.Transactions;
 using Google.Cloud.Functions.Framework;
+using Google.Cloud.Functions.Hosting;
 using Microsoft.AspNetCore.Http;
 using System.Threading.Tasks;
 
@@ -7,17 +10,44 @@ namespace CloudHttpFunction
 {
     public class Function : IHttpFunction
     {
+
+        private IPessoaRepository _pessoaRepository;
+        private IUnitOfWork _iUnitOfWork;
+
+        public Function(IPessoaRepository pessoaRepository, IUnitOfWork iUnitOfWork)
+        {
+            _pessoaRepository = pessoaRepository;
+            _iUnitOfWork = iUnitOfWork;
+        }
+
+        public class Startup : FunctionsStartup
+        {
+            // Virtual methods in the base class are overridden
+            // here to perform customization.
+        }
+
+        //ICloudEventFunction
+        /*
+         Uma função acionada por HTTP, que você invoca de solicitações HTTP padrão.
+         Uma função direcionada a eventos, que você usa para processar eventos da infraestrutura do Cloud, 
+         como mensagens em um tópico do Cloud Pub/Sub ou alterações em um bucket do Cloud Storage.
+         */
         /// <summary>
         /// Logic for your function goes here.
         /// </summary>
         /// <param name="context">The HTTP context, containing the request and the response.</param>
         /// <returns>A task representing the asynchronous operation.</returns>
+        /// [FunctionsStartup(typeof(Startup))]
         public async Task HandleAsync(HttpContext context)
         {
             Pessoa pessoa = new Pessoa();
 
             pessoa.Nome = "Bernhoeft";
             pessoa.DataNascimento = System.DateTime.Now;
+
+            _iUnitOfWork.BeginTransaction();
+            _pessoaRepository.Add(pessoa);
+            _iUnitOfWork.Commit();
             await context.Response.WriteAsync("Hello, Functions Framework da BernhoeftFunctions. " + pessoa.Nome + " DataNascimento: " + pessoa.DataNascimento);
         }
     }
